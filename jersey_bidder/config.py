c7d2ecffbbc2039b5dfeff8008dd9e058386c498
weiboon <lumweiboon@gmail.com>
2019-09-03 23:13:05 +0800

    Database Model Setup

--- jersey_bidder/config.py
+++ jersey_bidder/config.py
@@ -3,4 +3,5 @@ import os
 #config database uri here
 class Config:
     SECRET_KEY = 'c9970460fc2c3ad324add53c94e3bc2a'
-    SQLALCHEMY_DATABASE_URI = 'postgres://bobby:zmq,bs2008@localhost:5432/jersey_bidder'
+    # SQLALCHEMY_DATABASE_URI = 'postgres://bobby:zmq,bs2008@localhost:5432/jersey_bidder'
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost:5432/jersey_bidder'
--- jersey_bidder/models.py
+++ jersey_bidder/models.py
@@ -4,18 +4,25 @@ from flask_login import UserMixin
 from datetime import datetime
 
 class User(db.Model):
-    __table_args__ = {'extend_existing': True}   
-    roomNumber = db.Column(db.String(20), primary_key=True)
+    __tablename__ = 'user'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)   
+    roomNumber = db.Column(db.String(20))
     year = db.Column(db.Integer, nullable=False)
     points = db.Column(db.Integer, nullable=False)
     gender = db.Column(db.String(5), nullable=False)
 
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
     #foreign key constraint
     choice = db.relationship('Choice', backref='user', lazy=True)
+    sports = db.relationship('Sport', secondary='userSports', lazy=True)
+    jerseyNumber = db.relationship('JerseyNumber', backref='user', lazy=True)
+
 
 class Choice(db.Model):
+    __tablename__ = 'choice'
     __table_args__ = {'extend_existing': True}     
     id = db.Column(db.Integer, primary_key=True)
     submitDatetime = db.Column(db.DateTime, nullable=False)
@@ -26,7 +33,40 @@ class Choice(db.Model):
     fifthChoice = db.Column(db.Integer, nullable=False)
 
     #foreign key constraint
-    roomNumber = db.Column(db.String(20), db.ForeignKey('user.roomNumber'))
+    # roomNumber = db.Column(db.String(20), db.ForeignKey('user.roomNumber'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Sport(db.Model):
+    __tablename__ = 'sport'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    sportName = db.Column(db.String(100), nullable=False)
+
+    # relationship / foreign key constraints
+    Users = db.relationship('User', secondary='userSports')
+
+# UserSports is a junction table between Sport and User
+class UserSports(db.Model):
+    __tablename__ = 'userSports'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    # foreign key constraints
+    sport_id = db.Column(db.Integer, db.ForeignKey('sport.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+# male 0-99 female 100-199 (100 bias for female)
+class JerseyNumber(db.Model):
+    __tablename__ = 'jerseyNumber'
+    __table_args__ = {'extend_existing': True}
+    number = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    isTaken = db.Column(db.Boolean, default=False, nullable=False)
+
+    #relationship / foreign key constraint
+    # one to many relationship (one number can have multiple users)
+    users = db.relationship('User', lazy=True)
+
+
     
 #To do in documentation! initialize/changing databse models:
 """
