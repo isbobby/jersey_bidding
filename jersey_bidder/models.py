abeb1ef3dc37fe8b5a44264b1be3cc7b2e624716
weiboon <lumweiboon@gmail.com>
2019-09-06 17:38:15 +0800

    Update Models (haven't test)

--- jersey_bidder/models.py
+++ jersey_bidder/models.py
@@ -10,16 +10,22 @@ class User(db.Model, UserMixin):
     roomNumber = db.Column(db.String(20))
     year = db.Column(db.Integer, nullable=False)
     points = db.Column(db.Integer, nullable=False)
-    gender_id = db.Column(db.Integer, db.ForeignKey('gender.id'), nullable=False)
 
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
-    #foreign key constraint
+    # Foreign key constraints (only can have one)
+    gender_id = db.Column(db.Integer, db.ForeignKey('gender.id'), nullable=False)
+    preference_id = db.Column(db.Integer, db.ForeignKey('jerseyNumber.id'))
+    jerseyNumber_id = db.Column(db.Integer, db.ForeignKey('jerseyNumber.id'))
+
+    # Relationships
     gender = db.relationship('Gender', back_populates="User")
     choice = db.relationship('Choice', backref='user', lazy=True)
     sports = db.relationship('Sport', secondary='userSports', lazy=True)
     jerseyNumber = db.relationship('JerseyNumber', backref='user', lazy=True)
+    preference = db.relationship('JerseyNumber', backref='user', lazy=True)
+
 
 class Gender(db.Model):
     __tablename__ = 'gender'
@@ -52,14 +58,19 @@ class Sport(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sportName = db.Column(db.String(100), nullable=False)
 
-    # relationship / foreign key constraints
+    #foreign key constraints
+    gender_id = db.Column(db.Integer, db.ForeignKey('gender.id'), nullable=False)
+
+    # relationships
     Users = db.relationship('User', secondary='userSports')
+    Gender = db.relationship('Gender', back_populates="Sport"
 
 # UserSports is a junction table between Sport and User
 class UserSports(db.Model):
     __tablename__ = 'userSports'
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
+
     # foreign key constraints
     sport_id = db.Column(db.Integer, db.ForeignKey('sport.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -68,16 +79,17 @@ class UserSports(db.Model):
 class JerseyNumber(db.Model):
     __tablename__ = 'jerseyNumber'
     __table_args__ = {'extend_existing': True}
-    number = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    number = db.Column(db.Integer, nullable=False)
     isTaken = db.Column(db.Boolean, default=False, nullable=False)
 
     gender_id = db.Column(db.Integer, db.ForeignKey('gender.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     #relationship / foreign key constraint
     # one to many relationship (one number can have multiple users)
-    users = db.relationship('User', lazy=True)
+    users = db.relationship('User', back_populates="JerseyNumber", lazy=True)
     gender = db.relationship('Gender', back_populates="JerseyNumber")
+    preferences = db.relationship('User', lazy=True)
 
     
 
