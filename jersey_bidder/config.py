23037ceb216a0c733b1b03487dd1e4308e0524f8
isbobby <bobbyclex8@gmail.com>
2019-09-23 01:49:18 +0800

    configed

--- jersey_bidder/config.py
+++ jersey_bidder/config.py
@@ -4,8 +4,8 @@ import os
 class Config:
     SECRET_KEY = 'c9970460fc2c3ad324add53c94e3bc2a'
     #SQLALCHEMY_DATABASE_URI = 'postgres://bobby:zmq,bs2008@localhost:5432/jersey_bidder'
-    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost:5432/jersey_bidder'
-    # SQLALCHEMY_DATABASE_URI = 'postgres://nzlujsgydsuhub:3c28336b8b39e40c0a6a62c1d2c60cfdee2c169319d7ab6f67f280d60bbaa3b3@ec2-54-235-96-48.compute-1.amazonaws.com:5432/dd653s303qd6jm'
+    #SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost:5432/jersey_bidder'
+    SQLALCHEMY_DATABASE_URI = 'postgres://nzlujsgydsuhub:3c28336b8b39e40c0a6a62c1d2c60cfdee2c169319d7ab6f67f280d60bbaa3b3@ec2-54-235-96-48.compute-1.amazonaws.com:5432/dd653s303qd6jm'
     USER_APP_NAME = "Jersey Bidder"      # Shown in and email templates and page footers
     USER_ENABLE_EMAIL = False      # Disable email authentication
     USER_ENABLE_USERNAME = True    # Enable username authentication
